ee2ce35671b47fc41b8a007938c383696eb87969
Victor Magalhães <vicmagalhaes20@gmail.com>
2025-06-11 17:53:38 -0300

    adding content

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col">
           {/* Navbar */}
-          <div className="navbar bg-base-300 w-full">
+          <div className="navbar w-full">
             <div className="flex-none lg:hidden">
               <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                 <svg
@@ -28,20 +28,41 @@ function App() {
             <div className="hidden flex-none lg:block">
               <ul className="menu menu-horizontal">
                 {/* Navbar menu content here */}
-                <li><a>Navbar Item 1</a></li>
-                <li><a>Navbar Item 2</a></li>
+                <li><a>Sobre mim</a></li>
+                <li><a>Projetos</a></li>
+                <li><a>Contatos</a></li>
+                <li><a>GitHub</a></li>
               </ul>
             </div>
           </div>
-          {/* Page content here */}
-          Content
+          {/* Content */}
+          <main className="p-4">
+            <div className="hero">
+              <div className="hero-content flex-col lg:flex-row">
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                  className="max-w-sm rounded-full shadow-2xl"
+                />
+                <div>
+                  <h1 className="text-5xl font-bold">Box Office News!</h1>
+                  <p className="py-6">
+                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                  </p>
+                  <button className="btn btn-primary">Get Started</button>
+                </div>
+              </div>
+            </div>
+          </main>
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
           <ul className="menu bg-base-200 min-h-full w-80 p-4">
             {/* Sidebar content here */}
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
+            <li><a>Sobre mim</a></li>
+            <li><a>Projetos</a></li>
+            <li><a>Contatos</a></li>
+            <li><a>GitHub</a></li>
           </ul>
         </div>
       </div>
