0dda2c03553cd2bcc1b49c1e450d2d66ffca8fcf
VictorVM7 <vicmagalhaes20@gmail.com>
2025-06-19 19:53:20 -0300

    adjusting skills section

--- src/blocks/SkillsBlock.tsx
+++ src/blocks/SkillsBlock.tsx
@@ -2,18 +2,27 @@ import Skills from "../data/Skills.json"
 
 export default function SkillsBlock() {
     return (
-        <div id="skills" className="min-h-screen flex flex-col justify-center">
-            <div className="flex flex-row gap-5">
-                {
-                    Skills.map((skill) => {
-                        return (
-                            <div>
-                                <img className="grayscale hover:grayscale-0 transition duration-300" src={skill.image} width={50} />
-                            </div>
-                        )
-                    })
-                }
+        <div id="skills" className="flex flex-row justify-center">
+            <div className="flex flex-col gap-5">
+                <div>
+                    <h1 className="text-2xl menu-title">Front-End</h1>
+                </div>
+                <div className="flex flex-row gap-5">
+                    {
+                        Skills.frontend.map((skill) => {
+                            return (
+                                <div>
+                                    <img className="grayscale hover:grayscale-0 transition duration-300" src={skill.image} width={50} />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
+
+
+            <div className="divider divider-horizontal" />
+
         </div>
     );
 }
