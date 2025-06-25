c54205512f74fdd9f8125db3c4dea53d77e1a8d3
Victor Magalhães <vicmagalhaes20@gmail.com>
2025-06-25 17:47:41 -0300

    addind text to skills

--- src/blocks/SkillsBlock.tsx
+++ src/blocks/SkillsBlock.tsx
@@ -2,9 +2,10 @@ import Skills from "../data/Skills.json"
 
 export default function SkillsBlock() {
     return (
-        <div id="skills">
-            <div className="flex justify-center">
-                <h1 className="text-5xl font-bold">Skills</h1>
+        <div id="skills" className="flex flex-col items-center gap-5">
+            <div className="flex flex-col items-center gap-3">
+                <h1 className="text-5xl font-bold">Skills & Frameworks</h1>
+                <p>These are some Frameworks and Tools I have worked and I have knowledge, capability and skills to work with.</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
                 {
@@ -18,7 +19,7 @@ export default function SkillsBlock() {
                                     skills.map((skill) => {
                                         return (
                                             <div>
-                                                <img className="grayscale hover:grayscale-0 transition duration-300 rounded-sm" src={skill.image} width={50} />
+                                                <img className="grayscale hover:grayscale-0 transition duration-300 rounded-sm" src={skill.image} width={40} />
                                             </div>
                                         )
                                     })
