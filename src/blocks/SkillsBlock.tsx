6fc4cbd86436df1351ba64b3ab64c1b207773e1b
VictorVM7 <vicmagalhaes20@gmail.com>
2025-06-26 23:49:38 -0300

    adding tabs to skills

--- src/blocks/SkillsBlock.tsx
+++ src/blocks/SkillsBlock.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { Modules } from "../data/Skills";
 
 export default function SkillsBlock() {
+
+    const [TabSkillSelected, SetTabSkillSelected] = useState("Front End");
+
     return (
         <div id="skills" className="flex flex-col items-center gap-10">
 
@@ -9,37 +13,41 @@ export default function SkillsBlock() {
                 <p>These are some Frameworks and Tools I have worked and I have knowledge, capability and skills to work with.</p>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-16">
-                {
-                    Modules.map((module) => (
-                        <div key={module.name} className="flex flex-col">
+            <div className="flex items-center">
+                {/* name of each tab group should be unique */}
+                <div className="tabs tabs-lift">
+                    {
+                        Modules.map((module) => (
+                            <div key={module.name} className="flex flex-col">
+                                <input type="radio" name={module.name} className="tab" aria-label={module.name} checked={TabSkillSelected === module.name} onClick={() => SetTabSkillSelected(module.name)} />
+                                <div className="tab-content border-base-300 bg-base-100 p-10">
+                                    <div className="flex flex-col mb-5 gap-3">
+                                        <h1 className="text-2xl font-bold">{module.name}</h1>
+                                        <p>{module.description}</p>
+                                    </div>
 
-                            <div className="flex flex-col my-5 gap-3">
-                                <h1 className="text-2xl font-bold">{module.name}</h1>
-                                <p>{module.description}</p>
+                                    <div className="flex flex-wrap gap-5">
+                                        {
+                                            module.items.map((item) => (
+                                                <div key={item.name}>
+                                                    <div className="tooltip" data-tip={`${item.name}: ${item.description}`}>
+                                                        <img
+                                                            className="grayscale hover:grayscale-0 transition duration-300 rounded-sm"
+                                                            src={item.image}
+                                                            width={40}
+                                                            alt={item.name}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                </div>
                             </div>
-
-                            <div className="flex flex-wrap gap-5">
-                                {
-                                    module.items.map((item) => (
-                                        <div key={item.name}>
-                                            <div className="tooltip" data-tip={`${item.name}: ${item.description}`}>
-                                                <img
-                                                    className="grayscale hover:grayscale-0 transition duration-300 rounded-sm"
-                                                    src={item.image}
-                                                    width={40}
-                                                    alt={item.name}
-                                                />
-                                            </div>
-                                        </div>
-                                    ))
-                                }
-                            </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
-
         </div>
     );
 }
