9b49f497b776c7130d71877868b075bc1d7ac7b4
VictorVM7 <vicmagalhaes20@gmail.com>
2025-08-09 21:17:39 -0300

    adding download resume

--- src/components/pages/AboutMe.tsx
+++ src/components/pages/AboutMe.tsx
@@ -1,4 +1,6 @@
 import VictorPicture from "../../assets/images/personal/foto-victor.jpg"
+import PortugueseResume from "../../assets/docs/curriculo-victor-magalhaes.pdf"
+import EnglishResume from "../../assets/docs/resume-victor-magalhaes.pdf"
 
 // types/index.ts
 interface ContactInfo {
@@ -27,11 +29,6 @@ interface AboutMe {
 // components/ProfileCard.jsx
 const ProfileCard = ({ name, title, avatar }: ProfileData) => {
 
-	function OnClickDownloadResume(path: string) {
-		const url = `${window.location.origin}/src/assets/docs/${path}`
-		window.open(url, '_blank');
-	}
-
 	return (
 		<div className="rounded-3xl p-8 flex flex-col items-center text-center">
 			<div className="avatar mb-6">
@@ -45,46 +42,50 @@ const ProfileCard = ({ name, title, avatar }: ProfileData) => {
 				<p className="text-base-content/70 text-md mb-4 text-left">{title}</p>
 
 				<div className="flex flex-row gap-3">
-					<button className="btn btn-outline gap-2" onClick={() => OnClickDownloadResume("curriculo-victor-magalhaes.pdf")}>
-						<svg width={24} height={24} fill="none" viewBox="0 0 24 24">
-							<g clipPath="url(#BR_svg__a)">
-								<path d="M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12Z" fill="#6DA544" />
-								<path d="M12 4.696 21.913 12 12 19.305 2.087 12 12 4.696Z" fill="#FFDA44" />
-								<path d="M12 16.174a4.174 4.174 0 1 0 0-8.348 4.174 4.174 0 0 0 0 8.348Z" fill="#F0F0F0" />
-								<path
-									d="M9.913 11.74a7.02 7.02 0 0 0-2.086.315 4.173 4.173 0 0 0 7.59 2.34 7.034 7.034 0 0 0-5.504-2.655Zm6.183 1.06a4.174 4.174 0 0 0-7.932-2.447 8.607 8.607 0 0 1 7.932 2.447Z"
-									fill="#0052B4"
-								/>
-							</g>
-							<defs>
-								<clipPath id="BR_svg__a">
-									<path fill="#fff" d="M0 0h24v24H0z" />
-								</clipPath>
-							</defs>
-						</svg>
-						Resume
-					</button>
-					<button className="btn btn-outline gap-2" onClick={() => OnClickDownloadResume("resume-victor-magalhaes.pdf")}>
-						<svg width={24} height={24} fill="none" viewBox="0 0 24 24">
-							<g clipPath="url(#US_svg__a)">
-								<path d="M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12Z" fill="#F0F0F0" />
-								<path
-									d="M11.477 12H24a12.01 12.01 0 0 0-.413-3.13H11.478V12Zm0-6.262h10.761a12.064 12.064 0 0 0-2.769-3.13h-7.992v3.13ZM12 24c2.824 0 5.42-.976 7.47-2.609H4.53A11.948 11.948 0 0 0 12 24ZM1.761 18.26h20.477a11.93 11.93 0 0 0 1.348-3.13H.413c.3 1.116.758 2.167 1.348 3.13Z"
-									fill="#D80027"
-								/>
-								<path
-									d="M5.559 1.874h1.093l-1.017.739.389 1.196-1.018-.74-1.017.74.336-1.033c-.896.746-1.68 1.62-2.328 2.594h.35l-.647.47c-.1.168-.197.34-.29.513l.31.951-.578-.419C1 7.19.868 7.5.75 7.817l.34 1.048h1.258l-1.017.74.388 1.195-1.017-.739-.61.443C.033 10.994 0 11.494 0 12h12V0C9.63 0 7.42.688 5.559 1.874Zm.465 8.926-1.018-.739-1.017.739.389-1.196-1.017-.739h1.257l.388-1.195.389 1.195h1.257l-1.017.74.389 1.195Zm-.389-4.691.389 1.195-1.018-.739-1.017.74.389-1.196-1.017-.74h1.257l.388-1.195.389 1.196h1.257l-1.017.739Zm4.693 4.691-1.017-.739-1.017.739.388-1.196-1.017-.739h1.257l.389-1.195.388 1.195h1.258l-1.018.74.389 1.195Zm-.389-4.691.389 1.195-1.017-.739-1.017.74.388-1.196-1.017-.74h1.257l.389-1.195.388 1.196h1.258l-1.018.739Zm0-3.496.389 1.196-1.017-.74-1.017.74.388-1.196-1.017-.739h1.257L9.311.678l.388 1.196h1.258l-1.018.739Z"
-									fill="#0052B4"
-								/>
-							</g>
-							<defs>
-								<clipPath id="US_svg__a">
-									<path fill="#fff" d="M0 0h24v24H0z" />
-								</clipPath>
-							</defs>
-						</svg>
-						Resume
-					</button>
+					<a href={`${PortugueseResume}`} download target="_blank">
+						<button className="btn btn-outline gap-2">
+							<svg width={24} height={24} fill="none" viewBox="0 0 24 24">
+								<g clipPath="url(#BR_svg__a)">
+									<path d="M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12Z" fill="#6DA544" />
+									<path d="M12 4.696 21.913 12 12 19.305 2.087 12 12 4.696Z" fill="#FFDA44" />
+									<path d="M12 16.174a4.174 4.174 0 1 0 0-8.348 4.174 4.174 0 0 0 0 8.348Z" fill="#F0F0F0" />
+									<path
+										d="M9.913 11.74a7.02 7.02 0 0 0-2.086.315 4.173 4.173 0 0 0 7.59 2.34 7.034 7.034 0 0 0-5.504-2.655Zm6.183 1.06a4.174 4.174 0 0 0-7.932-2.447 8.607 8.607 0 0 1 7.932 2.447Z"
+										fill="#0052B4"
+									/>
+								</g>
+								<defs>
+									<clipPath id="BR_svg__a">
+										<path fill="#fff" d="M0 0h24v24H0z" />
+									</clipPath>
+								</defs>
+							</svg>
+							Resume
+						</button>
+					</a>
+					<a href={`${EnglishResume}`} download target="_blank">
+						<button className="btn btn-outline gap-2">
+							<svg width={24} height={24} fill="none" viewBox="0 0 24 24">
+								<g clipPath="url(#US_svg__a)">
+									<path d="M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12Z" fill="#F0F0F0" />
+									<path
+										d="M11.477 12H24a12.01 12.01 0 0 0-.413-3.13H11.478V12Zm0-6.262h10.761a12.064 12.064 0 0 0-2.769-3.13h-7.992v3.13ZM12 24c2.824 0 5.42-.976 7.47-2.609H4.53A11.948 11.948 0 0 0 12 24ZM1.761 18.26h20.477a11.93 11.93 0 0 0 1.348-3.13H.413c.3 1.116.758 2.167 1.348 3.13Z"
+										fill="#D80027"
+									/>
+									<path
+										d="M5.559 1.874h1.093l-1.017.739.389 1.196-1.018-.74-1.017.74.336-1.033c-.896.746-1.68 1.62-2.328 2.594h.35l-.647.47c-.1.168-.197.34-.29.513l.31.951-.578-.419C1 7.19.868 7.5.75 7.817l.34 1.048h1.258l-1.017.74.388 1.195-1.017-.739-.61.443C.033 10.994 0 11.494 0 12h12V0C9.63 0 7.42.688 5.559 1.874Zm.465 8.926-1.018-.739-1.017.739.389-1.196-1.017-.739h1.257l.388-1.195.389 1.195h1.257l-1.017.74.389 1.195Zm-.389-4.691.389 1.195-1.018-.739-1.017.74.389-1.196-1.017-.74h1.257l.388-1.195.389 1.196h1.257l-1.017.739Zm4.693 4.691-1.017-.739-1.017.739.388-1.196-1.017-.739h1.257l.389-1.195.388 1.195h1.258l-1.018.74.389 1.195Zm-.389-4.691.389 1.195-1.017-.739-1.017.74.388-1.196-1.017-.74h1.257l.389-1.195.388 1.196h1.258l-1.018.739Zm0-3.496.389 1.196-1.017-.74-1.017.74.388-1.196-1.017-.739h1.257L9.311.678l.388 1.196h1.258l-1.018.739Z"
+										fill="#0052B4"
+									/>
+								</g>
+								<defs>
+									<clipPath id="US_svg__a">
+										<path fill="#fff" d="M0 0h24v24H0z" />
+									</clipPath>
+								</defs>
+							</svg>
+							Resume
+						</button>
+					</a>
 				</div>
 			</div>
 		</div>
